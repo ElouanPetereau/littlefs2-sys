668902fb3273dfb5e8a889a539285cef1c6b6d61
ElouanPetereau <elouan.p@gmail.com>
2023-09-28 18:22:29 +0200

    Add static inline impl of lfs_crc

--- src/littlefs_patches/lfs_util_sys.h
+++ src/littlefs_patches/lfs_util_sys.h
@@ -206,8 +206,36 @@ static inline uint32_t lfs_tobe32(uint32_t a) {
     return lfs_frombe32(a);
 }
 
-// Calculate CRC-32 with polynomial = 0x04c11db7
-uint32_t lfs_crc(uint32_t crc, const void *buffer, size_t size);
+// Calculate CRC-32 with polynomial = 0x04c11db7 (copied from littlefs/lfs_util.c )
+static inline uint32_t lfs_crc(uint32_t crc, const void *buffer, size_t size) {
+    static const uint32_t rtable[16] = {
+        0x00000000,
+        0x1db71064,
+        0x3b6e20c8,
+        0x26d930ac,
+        0x76dc4190,
+        0x6b6b51f4,
+        0x4db26158,
+        0x5005713c,
+        0xedb88320,
+        0xf00f9344,
+        0xd6d6a3e8,
+        0xcb61b38c,
+        0x9b64c2b0,
+        0x86d3d2d4,
+        0xa00ae278,
+        0xbdbdf21c,
+    };
+
+    const uint8_t *data = buffer;
+
+    for (size_t i = 0; i < size; i++) {
+        crc = (crc >> 4) ^ rtable[(crc ^ (data[i] >> 0)) & 0xf];
+        crc = (crc >> 4) ^ rtable[(crc ^ (data[i] >> 4)) & 0xf];
+    }
+
+    return crc;
+}
 
 // Allocate memory, only used if buffers are not provided to littlefs
 // Note, memory must be 64-bit aligned
