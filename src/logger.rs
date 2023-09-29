f97e3794713de36fa916d5deb150dc4c1e81df60
ElouanPetereau <elouan.p@gmail.com>
2023-09-29 15:27:12 +0200

    Add a double peek struct + Support new logged types

--- src/logger.rs
+++ src/logger.rs
@@ -24,8 +24,8 @@ impl From<LittleFsLogLevel> for Level {
 }
 
 #[no_mangle]
-unsafe extern "C" fn log_msg(log_level: LittleFsLogLevel, message: *const u8, mut args: ...) {
-    let message_cstr = CStr::from_ptr(message as *const i8)
+unsafe extern "C" fn log_msg(log_level: LittleFsLogLevel, message: *const i8, mut args: ...) {
+    let message_cstr = CStr::from_ptr(message)
         .to_str()
         .expect("Should be able to convert the C string to rust");
     log::log!(log_level.into(), "{}", message_cstr);
@@ -36,19 +36,35 @@ unsafe extern "C" fn log_msg(log_level: LittleFsLogLevel, message: *const u8, mu
         match extracted_type {
             ArgType::Integer => {
                 let arg = list.arg::<i64>();
-                log::log!(log_level.into(), "Argument {i}: {arg}");
+                log::log!(log_level.into(), "Argument {i}({extracted_type:?}): {arg}");
             }
             ArgType::UnsignedInteger => {
                 let arg = list.arg::<u64>();
-                log::log!(log_level.into(), "Argument {i}: {arg}");
+                log::log!(log_level.into(), "Argument {i}({extracted_type:?}): {arg}");
             }
             ArgType::UnsignedIntegerHex => {
                 let arg = list.arg::<u64>();
-                log::log!(log_level.into(), "Argument {i}: 0x{arg:X}");
+                log::log!(
+                    log_level.into(),
+                    "Argument {i}({extracted_type:?}): 0x{arg:X}"
+                );
             }
             ArgType::FloatingPoint => {
                 let arg = list.arg::<f64>();
-                log::log!(log_level.into(), "Argument {i}: {arg}");
+                log::log!(log_level.into(), "Argument {i}({extracted_type:?}): {arg}");
+            }
+            ArgType::String => {
+                let arg = CStr::from_ptr(list.arg::<*const u8>() as *const i8)
+                    .to_str()
+                    .expect("Should be able to convert the C string to rust");
+                log::log!(log_level.into(), "Argument {i}({extracted_type:?}): {arg}");
+            }
+            ArgType::Pointer => {
+                let arg = list.arg::<*const usize>();
+                log::log!(
+                    log_level.into(),
+                    "Argument {i}({extracted_type:?}): {arg:?}"
+                );
             }
             _ => {
                 log::log!(
@@ -57,6 +73,49 @@ unsafe extern "C" fn log_msg(log_level: LittleFsLogLevel, message: *const u8, mu
                 );
             }
         };
+    }
+}
+
+#[derive(Debug, Clone)]
+struct DoublePeekIterator<I: Iterator> {
+    iter: I,
+    peeked1: Option<I::Item>,
+    peeked2: Option<I::Item>,
+}
+
+impl<I: Iterator> DoublePeekIterator<I> {
+    fn new(iter: I) -> Self {
+        DoublePeekIterator {
+            iter,
+            peeked1: None,
+            peeked2: None,
+        }
+    }
+
+    fn peek(&mut self) -> Option<&I::Item> {
+        if self.peeked1.is_none() {
+            self.peeked1 = self.iter.next();
+        }
+        self.peeked1.as_ref()
+    }
+
+    fn peek2(&mut self) -> Option<&I::Item> {
+        let _ = self.peek();
+        if self.peeked2.is_none() {
+            self.peeked2 = self.iter.next();
+        }
+        self.peeked2.as_ref()
+    }
+}
+
+impl<I: Iterator> Iterator for DoublePeekIterator<I> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let item = self.peeked1.take();
+        self.peeked1 = self.peeked2.take();
+        self.peeked2 = None;
+        item.or_else(|| self.iter.next())
     }
 }
 
@@ -77,11 +136,25 @@ pub enum ArgType {
 fn extract_types<const Size: usize>(format_string: &str) -> Vec<ArgType, Size> {
     let mut types = Vec::<ArgType, Size>::new();
 
-    let mut chars = format_string.chars().peekable();
+    let mut chars = DoublePeekIterator::new(format_string.chars());
     while let Some(c) = chars.next() {
+        if chars.clone().count() < 2 {
+            break;
+        }
         if c == '%' {
             if let Some(&next_char) = chars.peek() {
                 let arg_type = match next_char {
+                    'l' => {
+                        if let Some(&next_char) = chars.peek2() {
+                            match next_char {
+                                'd' | 'i' => ArgType::Integer,
+                                'u' | 'o' => ArgType::UnsignedInteger,
+                                _ => ArgType::Unknown,
+                            }
+                        } else {
+                            ArgType::Unknown
+                        }
+                    }
                     'd' | 'i' => ArgType::Integer,
                     'u' | 'o' => ArgType::UnsignedInteger,
                     'x' | 'X' => ArgType::UnsignedIntegerHex,
